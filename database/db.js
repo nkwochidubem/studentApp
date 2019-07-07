f04f3575ccc0cdfb5578c317e370792b86766fd1
nkwochidubem <nkwochidubem@yahoo.com>
2019-07-07 09:51:07 +0100

    changed the Mongo DB URL

--- database/db.js
+++ database/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-  db: 'mongodb://localhost:27017/angular8mean'
+  // db: 'mongodb://localhost:27017/angular8mean'
+  db: 'mongodb+srv://chidubem:icui4cu5517@studentappcluster-dakkc.mongodb.net/angular8mean'
 };
